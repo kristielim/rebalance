99f675a9261e772e487ebbb1c3be9b3b8cbf3feb
Rajeshwari Jadhav <rajeshwari@s-164-67-65-204.resnet.ucla.edu>
2018-10-20 16:36:48 -0700

    Remove unnecessary code

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const prereqs = {
-    'CS1': {prereqs: 'none', coreqs: 'none'},
-    'CS31': {prereqs: 'none', coreqs: 'none'},
-    'Eng Comp 3': {prereqs: 'none', coreqs: 'none'},
-    'Math 31A': {prereqs: 'Math 30B', coreqs: 'none'}
-};
+// const prereqs = {
+//     'CS1': {prereqs: 'none', coreqs: 'none'},
+//     'CS31': {prereqs: 'none', coreqs: 'none'},
+//     'Eng Comp 3': {prereqs: 'none', coreqs: 'none'},
+//     'Math 31A': {prereqs: 'Math 30B', coreqs: 'none'}
+// };
 
-//const classes = ['CS1', 'CS31', 'Eng Comp 3', 'Math 31A', 'CS1', 'CS31', 'Eng Comp 3', 'Math 31A', 'CS1', 'CS31', 'Eng Comp 3', 'Math 31A', 'CS1', 'CS31', 'Eng Comp 3', 'Math 31A'];
+
 
 const allClasses = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44];
 
-const doneClasses = [0, 1, 2, 5, 6];
+const doneClasses = [0, 1, 2, 5,];
 
 const classes = allClasses.filter( ( element ) => !doneClasses.includes( element ) );
 
