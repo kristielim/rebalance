32d4329b843dcf5d3628e3d07ce0848d4da82ef3
Kristie Lim <kristie.lim@gmail.com>
2018-11-18 11:48:51 -0800

    Add two conflicted classes test

--- schedule.test.js
+++ schedule.test.js
@@ -7,7 +7,7 @@ describe('Schedule', function () {
     it('should replace CS32 with EngComp3', function () {
       const oldSchedule = [['COM SCI 31', 'COM SCI 32'], ['ENGCOMP 3']];
       const newSchedule = schedule.replaceConflictedClasses(oldSchedule);
-      assert.deepEqual(newSchedule, [['COM SCI 31', 'ENGCOMP 3'], ['COM SCI 32']])
+      assert.deepEqual(newSchedule, [['COM SCI 31', 'ENGCOMP 3'], ['COM SCI 32']]);
     });
 
     it('should replace the conflicted class twice', function () {
@@ -15,7 +15,14 @@ describe('Schedule', function () {
       const newSchedule = schedule.replaceConflictedClasses(oldSchedule);
       assert.deepEqual(newSchedule, [['COM SCI 31', 'ENGCOMP 3'],
       ['COM SCI 32', 'COM SCI 1'],
-      ['COM SCI 33']])
+      ['COM SCI 33']]);
+    });
+
+    it('should replace two conflicted classes in the same quarter', function () {
+      const oldSchedule = [['COM SCI 31', 'COM SCI 32', 'COM SCI 33'], ['ENGCOMP 3', 'COM SCI 1']];
+      const newSchedule = schedule.replaceConflictedClasses(oldSchedule);
+      assert.deepEqual(newSchedule, [ [ 'COM SCI 31', 'ENGCOMP 3', 'COM SCI 1' ],
+      [ 'COM SCI 32', 'COM SCI 33' ] ]);
     });
 
   });
