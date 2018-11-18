2116a077e16a947398cd4d85b4e8a97750cd90c5
Kristie Lim <kristie.lim@gmail.com>
2018-11-18 10:56:23 -0800

    Reformat and break when no conflicts

--- schedule.js
+++ schedule.js
@@ -1,8 +1,8 @@
 const prereqs = {
-    'ENGCOMP 3': {prereqs: [], coreqs: 'none'},
-    'COM SCI 31': {prereqs: [], coreqs: 'none'},
-    'COM SCI 32': {prereqs: ['COM SCI 31'], coreqs: 'none'},
-    'COM SCI 33': {prereqs: ['COM SCI 32'], coreqs: 'none'},
+  'ENGCOMP 3': { prereqs: [], coreqs: 'none' },
+  'COM SCI 31': { prereqs: [], coreqs: 'none' },
+  'COM SCI 32': { prereqs: ['COM SCI 31'], coreqs: 'none' },
+  'COM SCI 33': { prereqs: ['COM SCI 32'], coreqs: 'none' },
 };
 
 // assume these are in order
@@ -10,98 +10,102 @@ const allClasses = ['COM SCI 1', 'COM SCI 31', 'ENGCOMP 3', 'MATH 31A', 'COM SCI
 
 const doneClasses = ['ENGCOMP 3', 'MATH 31A', 'MATH 31B'];
 
-const classes = allClasses.filter( ( element ) => !doneClasses.includes( element ) );
+const classes = allClasses.filter((element) => !doneClasses.includes(element));
 
 const numQuarters = 12;
 
 // checks if there is a conflict between classes within a quarter
 // returns a list of classes that need to be swapped out
 const findConflict = (quarterRow) => {
-    const conflictList = []
-    // for each class in the quarter
-    for (let i = 0; i < quarterRow.length; i++) {
-        const className = quarterRow[i];
-        // get prereqs for class
-        const classPrereqs = prereqs[className].prereqs;
+  const conflictList = []
+  // for each class in the quarter
+  for (let i = 0; i < quarterRow.length; i++) {
+    const className = quarterRow[i];
+    // get prereqs for class
+    const classPrereqs = prereqs[className].prereqs;
 
-        // if any of the prereqs are found in the same quarter, return the class
-        for (let j = 0; j < classPrereqs.length; j++) {
-            for (let k = 0; k < quarterRow.length; k++) {
-                if (classPrereqs[j] === quarterRow[k]) {
-                    conflictList.push(className);
-                    break;
-                }
-            }
+    // if any of the prereqs are found in the same quarter, return the class
+    for (let j = 0; j < classPrereqs.length; j++) {
+      for (let k = 0; k < quarterRow.length; k++) {
+        if (classPrereqs[j] === quarterRow[k]) {
+          conflictList.push(className);
+          break;
         }
+      }
     }
-    return conflictList;
+  }
+  return conflictList;
 };
 
 // return schedule with no conflicts
 const replaceConflictedClasses = (schedule) => {
-    // breaks if there is a conflict in the last quarter
-    // iterate through the schedule and look for prereq conflicts
-    for (let i = 0; i < schedule.length - 1; i++) {
-        // this is the list of classes for a particular quarter
-        const quarterRow = schedule[i];
-        const conflictClasses = findConflict(quarterRow);
-        const noConflictClasses = quarterRow.filter( ( element ) => !conflictClasses.includes( element ) );
+  // breaks if there is a conflict in the last quarter
+  // iterate through the schedule and look for prereq conflicts
+  for (let i = 0; i < schedule.length - 1; i++) {
+    // this is the list of classes for a particular quarter
+    const quarterRow = schedule[i];
+    const conflictClasses = findConflict(quarterRow);
+    const noConflictClasses = quarterRow.filter((element) => !conflictClasses.includes(element));
 
-        conflictClasses.forEach((conflictClass) => {
-            // swap with class in next quarter
-            const nextQuarterRow = schedule[i + 1];
-            for (let j = 0; j < nextQuarterRow.length; j++) {
-                const nextQuarterClass = nextQuarterRow[j];
-                // check if class can be swapped
-                const rowAfterSwap = [...noConflictClasses, nextQuarterClass];
-                const conflicts = findConflict(rowAfterSwap)
-                if (conflicts.length === 0) {
-                    schedule[i] = rowAfterSwap;
-                    // note that this does not preserve order
-                    // splice replaces class in next quarter with the conflict class of the previous quarter
-                    nextQuarterRow.splice(j, 1, conflictClass);
-                    schedule[i + 1] = nextQuarterRow;
-                }
-            }
-        });
-    }
-    return schedule;
+    conflictClasses.forEach((conflictClass) => {
+      // swap with class in next quarter
+      const nextQuarterRow = schedule[i + 1];
+      for (let j = 0; j < nextQuarterRow.length; j++) {
+        const nextQuarterClass = nextQuarterRow[j];
+        // check if class can be swapped
+        const rowAfterSwap = [...noConflictClasses, nextQuarterClass];
+        const conflicts = findConflict(rowAfterSwap)
+        if (conflicts.length === 0) {
+          schedule[i] = rowAfterSwap;
+          // note that this does not preserve order
+          // splice replaces class in next quarter with the conflict class of the previous quarter
+          nextQuarterRow.splice(j, 1, conflictClass);
+          schedule[i + 1] = nextQuarterRow;
+          break;
+        }
+      }
+    });
+  }
+  return schedule;
 };
+
+const oldSchedule = [['COM SCI 31', 'COM SCI 32'], ['ENGCOMP 3', 'COM SCI 33']];
+console.log(replaceConflictedClasses(oldSchedule));
 
 // input classes
 const createSchedule = (classes) => {
-    // return an array that will contain classes partitioned by quarters
-    let schedule = [];
-    
-    let maxClasses = Math.ceil(classes.length / numQuarters);
-    let incompleteQuarters = numQuarters * maxClasses - classes.length;
-    let completeQuarters = numQuarters - incompleteQuarters;
+  // return an array that will contain classes partitioned by quarters
+  let schedule = [];
 
-    // start classes per quarter as max classes
-    let classesPerQuarter = maxClasses;
+  let maxClasses = Math.ceil(classes.length / numQuarters);
+  let incompleteQuarters = numQuarters * maxClasses - classes.length;
+  let completeQuarters = numQuarters - incompleteQuarters;
 
-    // iterate through list of classes
-    for (let i = 0; i < classes.length;) {
+  // start classes per quarter as max classes
+  let classesPerQuarter = maxClasses;
 
-        // once all complete quarters have been filled, start using one less class per quarter
-        if (i == completeQuarters * maxClasses) {
-            classesPerQuarter--;
-        }
+  // iterate through list of classes
+  for (let i = 0; i < classes.length;) {
 
-        // push list of classes into schedule
-        const quarter = classes.slice(i, i + classesPerQuarter);
-        schedule.push(quarter);
-        i += classesPerQuarter;
+    // once all complete quarters have been filled, start using one less class per quarter
+    if (i == completeQuarters * maxClasses) {
+      classesPerQuarter--;
     }
-    
-    schedule = replaceConflictedClasses(schedule);
 
-    return schedule;
+    // push list of classes into schedule
+    const quarter = classes.slice(i, i + classesPerQuarter);
+    schedule.push(quarter);
+    i += classesPerQuarter;
+  }
+
+  schedule = replaceConflictedClasses(schedule);
+
+  return schedule;
 };
 
 //console.log(createSchedule(classes));
 
 module.exports = {
-    replaceConflictedClasses,
-    createSchedule
+  replaceConflictedClasses,
+  createSchedule
 }
--- schedule.test.js
+++ schedule.test.js
@@ -2,13 +2,15 @@ var schedule = require('./schedule');
 var assert = require('assert');
 
 
-describe('Schedule', function() {
-  describe('#replaceConflictedClasses()', function() {
-    it('should replace CS32 with EngComp3', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-      const oldSchedule = [['COM SCI 31', 'COM SCI 32'],['ENGCOMP 3']];
+describe('Schedule', function () {
+  describe('#replaceConflictedClasses()', function () {
+    it('should replace CS32 with EngComp3', function () {
+      const oldSchedule = [['COM SCI 31', 'COM SCI 32'], ['ENGCOMP 3']];
       const newSchedule = schedule.replaceConflictedClasses(oldSchedule);
-      assert.deepEqual(newSchedule, [ [ 'COM SCI 31', 'ENGCOMP 3' ], [ 'COM SCI 32' ] ])
+      assert.deepEqual(newSchedule, [['COM SCI 31', 'ENGCOMP 3'], ['COM SCI 32']])
     });
+    it('should replace the conflicted class twice'), function () {
+      const oldSchedule = [['COM SCI 31', 'COM SCI 32'], ['ENGCOMP 3', 'COM SCI 33']];
+    }
   });
 });
